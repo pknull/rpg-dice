4d686b4c441dab257c8a678fb0f744fbe718a270
pknull <louis.grenzebach@gmail.com>
2018-12-11 21:05:39 -0700

    added calculated throw

--- dice_roller/DiceProbabilty.py
+++ dice_roller/DiceProbabilty.py
@@ -2,13 +2,50 @@ from fractions import Fraction
 from decimal import Decimal
 from collections import Counter
 import itertools as it
+from math import factorial
 from dice_roller.DiceParser import DiceParser
 import sympy
+
 
 class DiceProbability(object):
     parser = DiceParser()
 
-    def throw(self, dexp='1d1', target=4):
+    def calcThrow(self, dexp='1d1', target=2):
+        parsed_roll = self.parser.parse_input(dexp)
+        numberOfDice = int(parsed_roll['number_of_dice'])
+        numberOfSides = int(parsed_roll['sides'])
+        total_possibilities = numberOfSides ** numberOfDice
+
+        s_op = parsed_roll['s']['operator']
+        s_val = parsed_roll['s']['val']
+        f_op = parsed_roll['f']['operator']
+        f_val = parsed_roll['f']['val']
+
+        dice = Counter(range(1, numberOfSides + 1))
+
+        success_set = self.calc(s_op, s_val, dice)
+        fail_set = self.calc(f_op, f_val, dice)
+
+        success_probability = Fraction(len(success_set), numberOfSides)
+        die_probability = Fraction(len(fail_set), numberOfSides)
+
+        print('---- Counts')
+        print(total_possibilities)
+
+        print('---- Win')
+        chance = self.hit_chance(numberOfDice, target, success_probability)
+        print('{0:.2f}'.format(chance.numerator / Decimal(chance.denominator) * 100))
+
+        print('---- Fail')
+        chance = 1 - chance
+        print('{0:.2f}'.format(chance.numerator / Decimal(chance.denominator) * 100))
+
+        print('---- Die')
+        fail_target = round((numberOfDice / 2) + 1)
+        chance = self.hit_chance(numberOfDice, fail_target, die_probability)
+        print('{0:.2f}'.format(chance.numerator / Decimal(chance.denominator) * 100))
+
+    def bruteThrow(self, dexp='1d1', target=2):
         # parse
         parsed_roll = self.parser.parse_input(dexp)
         numberOfDice = int(parsed_roll['number_of_dice'])
@@ -20,20 +57,18 @@ class DiceProbability(object):
         f_val = parsed_roll['f']['val']
 
         dice = Counter(range(1, numberOfSides + 1))
-        possibilities = list(it.product(dice, repeat=numberOfDice))
-        total_possibilities = numberOfSides**numberOfDice
+        total_possibilities = numberOfSides ** numberOfDice
 
         success_set = self.calc(s_op, s_val, dice)
         fail_set = self.calc(f_op, f_val, dice)
-
-        print(parsed_roll)
 
         targetAmount = target
         totalSuccess = 0
         totalFails = 0
         totalCritFails = 0
+        fail_target = round((numberOfDice / 2) + 1)
 
-        for i in possibilities:
+        for i in it.product(dice, repeat=numberOfDice):
             successes = 0
             fails = 0
             totals = Counter(i)
@@ -42,7 +77,8 @@ class DiceProbability(object):
             for j in fail_set:
                 fails += totals[j]
 
-            if fails > (numberOfDice/2):
+            if fails >= fail_target:
+                totalFails += 1
                 totalCritFails += 1
             elif successes >= targetAmount:
                 totalSuccess += 1
@@ -53,24 +89,31 @@ class DiceProbability(object):
         fail = Fraction(totalFails, total_possibilities)
         crits = Fraction(totalCritFails, total_possibilities)
 
-        finalCount = sum([totalSuccess,totalFails,totalCritFails])
         print('---- Counts')
         print(total_possibilities)
-        print(finalCount)
         print('---- Win')
-        print(success_set)
-        print(totalSuccess)
         print('{0:.2f}'.format(win.numerator / Decimal(win.denominator) * 100))
         print('---- Fail')
-        print(fail_set)
-        print(totalFails)
         print('{0:.2f}'.format(fail.numerator / Decimal(fail.denominator) * 100))
         print('---- Die')
-        print(totalCritFails)
         print('{0:.2f}'.format(crits.numerator / Decimal(crits.denominator) * 100))
-
 
     def calc(self, op, val, space):
         """Subset of sample space for which a condition is true."""
         return {element for element in space if sympy.sympify(str(element) + op + val)}
 
+    def binomial(self, x, y):
+        try:
+            binom = factorial(x) // factorial(y) // factorial(x - y)
+        except ValueError:
+            binom = 0
+        return binom
+
+    def exact_hit_chance(self, n, k, p):
+        """Return the probability of exactly k hits from n dice"""
+        # a hit is a 5 or 6, so 1/3 chance.
+        return self.binomial(n, k) * (p) ** k * (1 - p) ** (n - k)
+
+    def hit_chance(self, n, k, p):
+        """Return the probability of at least k hits from n dice"""
+        return sum([self.exact_hit_chance(n, x, p) for x in range(k, n + 1)])
